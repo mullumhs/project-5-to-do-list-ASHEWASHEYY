35a2ebd3407fd5184067a8ef3786863118723af4
ASHEWASHEYY <bella.doodyharding@education.nsw.gov.au>
2024-05-28 04:38:35 +0000

    dice rooaalllarrrr LABS

--- lab1.py
+++ lab1.py
@@ -9,17 +9,31 @@
 
 # Create a list named favorite_foods and add at least five different food items to it
 
+favourite_foods = ['apple', 'chips', 'carrots', 'banana', 'chocolate']
+
+
 
 # Change the third item in the list to a different food
+
+favourite_foods = ['apple', 'chips', 'lasagna', 'banana', 'chocolate']
 
 
 # Print out only the first and fourth items in the list
 
+# print("apple", "banana")
+
 
 # Ask the user for a food item
+
+poo = input("whats ur fav food?!")
+
 
 
 # Append the user's food item to the list
 
+favourite_foods = ('apple', 'chips', 'carrots', 'banana', 'chocolate ' + poo)
 
 # Print out the entire list of food items
+
+print(favourite_foods)
+
